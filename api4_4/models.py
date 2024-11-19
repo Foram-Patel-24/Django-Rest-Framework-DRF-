6c6cd373d28c3bb5ff350e4132b66893b8487b65
Foram Patel <foramdelvadiya@gmail.com>
2024-11-19 13:19:52 +0530

    Delete api4_4 directory

--- api4_4/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student4_4(models.Model):
-    name = models.CharField(max_length=58)
-    roll = models.IntegerField()
-    city = models.CharField(max_length=58)
--- api4_4/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Student4_4
-
-class StudentSerializer4_4(serializers.ModelSerializer):
-    class Meta:
-        model = Student4_4
-        fields = ['id' , 'roll' , 'name' , 'city']
