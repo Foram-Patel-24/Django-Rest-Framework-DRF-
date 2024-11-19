bc1607ac11964b54942443f22f9106a10fc4783d
Foram Patel <foramdelvadiya@gmail.com>
2024-11-19 13:20:04 +0530

    Delete api3_3 directory

--- api3_3/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Student3_3
-
-class StudentSerializer3_3(serializers.ModelSerializer):
-    class Meta:
-        model = Student3_3
-        fields = ['id' , 'roll' , 'name' , 'city']
