20007c06b3f68210208e69fe9545877b603305aa
Foram Patel <foramdelvadiya@gmail.com>
2024-11-19 13:19:42 +0530

    Delete api4 directory

--- api4/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Student4
-from rest_framework import serializers
-
-class StudentSerializer4(serializers.ModelSerializer):
-    class Meta :
-        model = Student4
-        fields = ['id' , 'roll' , 'name' , 'city']
